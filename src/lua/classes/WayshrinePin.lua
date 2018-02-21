f89ed3245cb8c84dd7c19b03a10d805c1d1c6344
Martynas Petuska <martynas.petuska@gamma.co.uk>
2018-02-21 15:13:08 +0000

    [MyMiniMap-6] Extract mini map tiles to a separate class. Base inheritance implemented.

--- src/lua/classes/WayshrinePin.lua
+++ src/lua/classes/WayshrinePin.lua
@@ -3,53 +3,35 @@
   [] E-mail: martynas.petuska@outlook.com
   [] Date:   February 2018
 --]]
+local super = require "lua.classes.AbstractPin";
+
 --====================================================== CLASS =======================================================--
 ---A base class to hold common functionality shared between the various types of map pins.
 ---It is mostly intended to be extended by the pin-type-specific classes.
 --- 
----@class Pin
+---@class WayshrinePin
 ---@field public Position table
 ---@field public icon string
 ---@field public type number
 ---@field public zoneId number
 ---@field public enabled boolean
-local Pin = {};
+local WayshrinePin = super:New();
 --====================================================================================================================--
 
 ---Constructor
----@return Pin
-function Pin:New()
+---@return WayshrinePin
+function WayshrinePin:New()
 	local obj = setmetatable({}, { __index = self });
 	obj:Init();
 	return obj;
 end
 
 ---Initialises the new object.
-function Pin:Init()
-	self.Position = {
-		x = -1,
-		y = -1
-	};
-	self.icon = -1;
-	self.type = -1;
-	self.zoneId = -1;
-	self.enabled = true;
+function WayshrinePin:Init()
+	super.Init(self);
 end
 
 -- region Getters & Setters
----Gets the pin's normalised coordinates in the map.
----@return number, number
-function Pin:GetMapPos()
-	return self.Position.x, self.Position.y;
-end
-
----Sets the pin's normalised coordinates in the map.
----@param nX number
----@param nY number
-function Pin:SetMapPos(nX, nY)
-	self.Position.x = nX;
-	self.Position.y = nY;
-end
 -- endregion
 
-return Pin;
+return WayshrinePin;
