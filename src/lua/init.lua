bce400945eef28abb2b089b4724666fdd7962d33
Martynas Petuska <martynas.petuska@outlook.com>
2018-04-14 15:36:03 +0100

    [MyMiniMap-30] Incorrect building entering behaviour

--- src/lua/init.lua
+++ src/lua/init.lua
@@ -17,9 +17,7 @@ end
 ---@return void
 local function RegisterEvents()
 	EVENT_MANAGER:RegisterForEvent(ADDON.name .. "_ZoneChanged", EVENT_ZONE_CHANGED, EventHandlers.OnZoneChanged);
-	EVENT_MANAGER:RegisterForEvent(ADDON.name .. "_ZoneChangeds", EVENT_ZONE_UPDATED, function()
-		d("ZONE UPDATED")
-	end);
+	EVENT_MANAGER:RegisterForEvent(ADDON.name .. "_SubZoneChanged", EVENT_CURRENT_SUBZONE_LIST_CHANGED, EventHandlers.OnSubZoneChanged);
 	EVENT_MANAGER:RegisterForEvent(ADDON.name .. "_PlayerActivated", EVENT_PLAYER_ACTIVATED, EventHandlers.OnPlayerActivated);
 end
 
